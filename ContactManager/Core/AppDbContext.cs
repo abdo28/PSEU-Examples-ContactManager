471eabc63d83be21ede2c1c7c46ec456b99a592c
Ahmad Shnaiwer <ashnaiwer@apa.org>
2020-10-09 11:32:25 +0300

    Initial commit

---
+++ ContactManager/Core/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager.Core
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(AppSettings.ConnectionString);
+        }
+    }
+}
